03fbd37ff986883a819ab479a94db16ad00589ec
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2020-05-18 18:40:57 +0800

    Update email content and don't send email to the author of the post.

--- src/Console/Commands/NotifySubscribers.php
+++ src/Console/Commands/NotifySubscribers.php
@@ -37,16 +37,18 @@ class NotifySubscribers extends Command
 
         $pendingNotifications->each(function ($notification) {
             $notification->status = 'processing';
-            $notification->save();
+            // $notification->save();
 
             $post = $notification->post;
             $subscriptions = $post->thread->subscription;
 
             $subscriptions->each(function ($subscription) use ($post) {
-                Mail::to($subscription->user)->send(new NewPostCreated($post, $subscription->user));
+                if ($post->author_id !== $subscription->user_id) {
+                    Mail::to($subscription->user)->send(new NewPostCreated($post, $subscription->user));
+                }
             });
 
-            $notification->delete();
+            // $notification->delete();
         });
     }
 
--- views/emails/new_post.blade.php
+++ views/emails/new_post.blade.php
@@ -1,4 +1,8 @@
 @component('mail::message')
-    # Testing
+## New post from {{ $post->author->first_name  }} posted
 
+{!! $post->content !!}<br><br><br>
+
+<hr>
+<small>You are receiving this because you are subscribed to this thread. You can <a href="">unsubscribe</a> to this thread if you don't like receiving updates about the topic.</small>
 @endcomponent
